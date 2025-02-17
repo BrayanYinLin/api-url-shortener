6e8b3cd3704aa4ec23b087a076579b9b29d4e9fa
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-02-17 01:19:46 -0500

    fix: cors and preflight

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,24 @@ import express from 'express'
 import { PORT } from './lib/enviroment'
 import { routerAuth as authRouter } from './routers/auth.router'
 import { routerLink as linkRouter } from './routers/link.router'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import { corsOrigins, preflight } from './middlewares/cors-origins'
 
 const app = express()
 const port = Number(PORT) || 5373
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(corsOrigins())
+app.options('*', preflight())
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', authRouter)
 app.use('/api/link', linkRouter)
+app.disable('x-powered-by')
 
 const init = async () => {
   app.listen(port)
-  console.log('running at http://localhost:5373')
+  console.log(`Running...`)
 }
 
 init()
